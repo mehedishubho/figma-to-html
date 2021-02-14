c5fe263f9ee7e78d9534ed7ab0fb49dcfd614f7d
Mehedi Hassan Shubho <35875394+mehedishubho@users.noreply.github.com>
2021-02-15 04:00:20 +0600

    Add files via upload

---
+++ assets/js/slider.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:01,
+    nav:true,
+    stagePadding:0,
+    smartSpeed:750,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
